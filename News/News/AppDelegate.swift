f5ae45b26826e0b9274ed1ef53ef2b5040819c97
Ryan Lehman <rlehman221@gmail.com>
2018-08-03 22:10:49 -0400

    Fixed My_Feed, added persistent a login, and refactored notifications

--- News/News/AppDelegate.swift
+++ News/News/AppDelegate.swift
@@ -15,10 +15,6 @@ import FirebaseMessaging
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
 
     var window: UIWindow?
-    
-    
-
-    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -40,7 +36,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         application.registerForRemoteNotifications()
         
         FirebaseApp.configure()
+        setRootViewController()
         return true
+    }
+    
+    func setRootViewController() {
+        if Auth.auth().currentUser != nil {
+            // Set Your home view controller Here as root View Controller
+            let storyboard = UIStoryboard.init(name: "my_feed", bundle: nil)
+            
+            // controller identifier sets up in storyboard utilities
+            // panel (on the right), it called Storyboard ID
+            let viewController = storyboard.instantiateViewController(withIdentifier: "home_view") as! NavViewController
+            
+            
+            self.window?.rootViewController = viewController
+            self.window?.makeKeyAndVisible()
+        }
     }
     
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
--- News/News/Home_Page/My_Feeds_ViewController.swift
+++ News/News/Home_Page/My_Feeds_ViewController.swift
@@ -10,6 +10,7 @@ import Firebase
 
 class My_Feeds_ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
 
+    @IBOutlet weak var segemented_filters: UISegmentedControl!
     @IBOutlet weak var table_view: UITableView!
     
     @IBOutlet weak var search_bar: UISearchBar!
@@ -17,12 +18,15 @@ class My_Feeds_ViewController: UIViewController, UITableViewDataSource, UITableV
     @IBOutlet weak var filter_button: UIButton!
     
     @IBOutlet weak var filter_label: UILabel!
+    @IBOutlet weak var filter_view: UIView!
     
-    var feed_data = [Int : [String : String]] ()
+    //var feed_data = [Int : [String : String]] ()
+    var feed_data: [[String:String]] = []
     var clubHolder = [String]()
     var post_time = "";
     var counter2 = 0;
-    
+    var filter = "both"
+    var search_text = ""
     var refreshControl: UIRefreshControl!
     
     var ref: DatabaseReference! // Reference to database
@@ -32,13 +36,14 @@ class My_Feeds_ViewController: UIViewController, UITableViewDataSource, UITableV
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        let textFieldInsideSearchBar = search_bar.value(forKey: "searchField") as? UITextField
+        segemented_filters.addTarget(self, action: #selector(segmented_control_changed), for: .valueChanged)
+        textFieldInsideSearchBar?.backgroundColor = UIColor.gray
         
         table_view.dataSource = self
         table_view.delegate = self
         refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action:  #selector(refreshData), for: UIControlEvents.valueChanged)
-        //refreshControl.backgroundColor = UIColor.redColor
-        // refreshControl.tintColor = UIColor.yellowColor
         table_view.addSubview(refreshControl)
         loadClubs {
             self.load_database()
@@ -58,6 +63,30 @@ class My_Feeds_ViewController: UIViewController, UITableViewDataSource, UITableV
         refreshControl?.endRefreshing()
     }
     
+    @objc func segmented_control_changed()
+    {
+        let filter_data = segemented_filters.selectedSegmentIndex
+        
+        switch (filter_data){
+        case (0):
+            self.filter = "event"
+            self.refreshData()
+            break
+        case (1):
+            self.filter = "announcement"
+            self.refreshData()
+            break
+        case (2):
+            self.filter = "both"
+            self.refreshData()
+            break
+        default:
+            break
+        }
+        sleep(1)
+        filter_view.alpha = 0
+    }
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
@@ -71,9 +100,9 @@ class My_Feeds_ViewController: UIViewController, UITableViewDataSource, UITableV
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! My_Feed_TableViewCell
         
         // Use the outlet in our custom class to get a reference to the UILabel in the cell
-        cell.club_name.text = self.feed_data[indexPath.item]?["Name"]
-        cell.Subject.text = self.feed_data[indexPath.item]?["Subject"]
-        cell.time_duration.text = self.feed_data[indexPath.item]?["Duration"]
+        cell.club_name.text = self.feed_data[indexPath.item]["Name"]
+        cell.Subject.text = self.feed_data[indexPath.item]["Subject"]
+        cell.time_duration.text = self.feed_data[indexPath.item]["Duration"]
         cell.image_view.layer.masksToBounds = true
         cell.image_view.layer.cornerRadius = CGFloat(roundf(Float(4)))
         
@@ -92,6 +121,7 @@ class My_Feeds_ViewController: UIViewController, UITableViewDataSource, UITableV
     // Loads only clubs that the user has subscribed to into an array for later comparison
     func loadClubs (finished: @escaping () -> Void)
     {
+        self.clubHolder.removeAll()
         ref = Database.database().reference() // Reference to database
         let uid = Auth.auth().currentUser?.uid
         ref.child("Users").child(uid!).child("Subscribed").observeSingleEvent(of: .value) { (snapshot) in
@@ -117,34 +147,84 @@ class My_Feeds_ViewController: UIViewController, UITableViewDataSource, UITableV
             
             let posts = snapshot.value
             let all_posts = posts as? [String: String]
-            if (self.clubHolder.contains(all_posts!["Name"]!)){
-                
-                self.counter2 -= 1
-                self.feed_data[self.counter2] = all_posts
-                self.post_time = table_view_helper().timeDuration(date: self.feed_data[0]!["Date"]!)
-                self.feed_data[self.counter2]!["Duration"] = self.post_time
+
+            if (self.clubHolder.contains(all_posts!["Name"]!)) {
+                if (self.search_text == ""
+                    || all_posts!["Name"]?.lowercased().range(of: self.search_text.lowercased()) != nil
+                    || all_posts!["Subject"]?.lowercased().range(of: self.search_text.lowercased()) != nil
+                    || all_posts!["Body"]?.lowercased().range(of: self.search_text.lowercased()) != nil) {
+                    
+                    if (self.filter == "both"
+                        || (self.filter == "event" && all_posts!["Type"] == "Event")
+                        || (self.filter == "announcement" && all_posts!["Type"] == "Announcement")) {
+                        self.feed_data.insert(all_posts!, at: 0)
+                        self.post_time = table_view_helper().timeDuration(date: self.feed_data[0]["Date"]!)
+                        self.feed_data[0]["Duration"] = self.post_time
+                    }
+                }
             }
             self.table_view.reloadData()
-            print(self.feed_data.count)
         })
     }
     
     // Fix - Thread 1: Fatal error: Can't form Range with upperBound < lowerBound
     func reloadDuration()
     {
+        if (feed_data.count == 0) {
+            return
+        }
         for index in 0...(feed_data.count - 1) {
-            post_time = table_view_helper().timeDuration(date: self.feed_data[index]!["Date"]!)
-            self.feed_data[index]!["Duration"] = post_time
+            post_time = table_view_helper().timeDuration(date: self.feed_data[index]["Date"]!)
+            self.feed_data[index]["Duration"] = post_time
         }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?)
     {
         let dataToSend = segue.destination as? Post_PageViewController
-        dataToSend?.receivedPostData = (self.feed_data[selected_Post]?["Body"])!
-        dataToSend?.receivedName = (self.feed_data[selected_Post]?["Name"])!
-        dataToSend?.receivedSubject = (self.feed_data[selected_Post]?["Subject"])!
-        dataToSend?.receivedDate = (self.feed_data[selected_Post]?["Duration"])!
+        dataToSend?.receivedPostData = (self.feed_data[selected_Post]["Body"])!
+        dataToSend?.receivedName = (self.feed_data[selected_Post]["Name"])!
+        dataToSend?.receivedSubject = (self.feed_data[selected_Post]["Subject"])!
+        dataToSend?.receivedDate = (self.feed_data[selected_Post]["Duration"])!
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        search_bar.resignFirstResponder()
+        search_text = search_bar.text!
+        self.refreshData()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        search_text = search_bar.text!
+        self.refreshData()
+    }
+    
+    @IBAction func filter_pressed(_ sender: Any) {
+        if (filter_view.alpha == 1){
+            filter_view.alpha = 0
+            
+            let filter_data = segemented_filters.selectedSegmentIndex
+            
+            switch (filter_data){
+            case (0):
+                self.filter = "event"
+                self.refreshData()
+                break
+            case (1):
+                self.filter = "announcement"
+                self.refreshData()
+                break
+            case (2):
+                self.filter = "both"
+                self.refreshData()
+                break
+            default:
+                break
+            }
+            
+        } else {
+            filter_view.alpha = 1
+        }
     }
     
     override func didReceiveMemoryWarning()
